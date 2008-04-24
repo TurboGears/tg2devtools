d7210c8ca485f0936c3390617652c97ab2efff34
fredlin <none@none>
2008-04-24 05:42:33 +0000

    update setup.py
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/projects/tg.devtools/trunk%404481

--- setup.py
+++ setup.py
@@ -18,19 +18,19 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'tg', 'sqlalchemy-migrate >= 0.4.4', 
+        'TurboGears2', 'sqlalchemy-migrate >= 0.4.4', 
     ],
     entry_points='''
-        [paste.paster_create_template]
-        turbogears2=devtools.pastetemplate:TurboGearsTemplate
-        quickstart = devtools.commands.quickstart:QuickstartCommand
-        [turbogears2.template]
-        turbogears2=tgdevtools.pastetemplate:TurboGearsTemplate
-        [turbogears2.command]
+        [paste.global_paster_command]
         quickstart = devtools.commands.quickstart:QuickstartCommand
         [paste.paster_command]
-        migrate = devtoold.commands.migration:MigrateCommand
-        [turbogears2.command]
         migrate = devtools.commands.migration:MigrateCommand
+        [turbogears2.command]
+        quickstart = devtools.commands.quickstart:QuickstartCommand
+        migrate = devtools.commands.migration:MigrateCommand
+        [paste.paster_create_template]
+        turbogears2=devtools.pastetemplate:TurboGearsTemplate
+        [turbogears2.template]
+        turbogears2=devtools.pastetemplate:TurboGearsTemplate
     ''',
 )
