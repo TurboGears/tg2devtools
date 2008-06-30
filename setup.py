ae9539d29c7d259e5230c19c9f6dc03cc7d9d7e2
mramm <none@none>
2008-06-30 21:09:54 +0000

    Updates to setup.py
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/projects/tg.devtools/trunk%404848

--- setup.py
+++ setup.py
@@ -5,21 +5,22 @@ import sys, os
 
 setup(
     name='tg.devtools',
-    version="1.9.7",
+    version="1.9.7a1",
     description="",
     long_description="""""",
     classifiers=[],
     keywords='turbogears',
     author="TurboGears Team 2008",
-    author_email="",
+    author_email="turbogears@groups.google.com",
     url="www.turbogears.org",
     license="MIT",
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'TurboGears2', 'sqlalchemy-migrate >= 0.4.4',
-        'tw.forms', 'DBSprockets',
+    install_requires=['Pylons>=0.9.7beta5','TurboGears2==1.9.7a1', 
+        'SQLAlchemy>=0.5beta1', 'PEAK-Rules',
+        'sqlalchemy-migrate>=0.4.4', 'ToscaWidgets>=0.9', 
+        'tw.forms>=0.9', 'DBSprockets >= 0.2, <0.5',
         'tg.ext.repoze.who'
     ],
     entry_points='''
