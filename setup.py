055c48fae4304e449cba499ca36cd49b6296abf3
percious <none@none>
2008-06-30 21:39:42 +0000

    fixed template to be 0.5beta1 complient.  also, setup.py pulls in wsgiref so it is there already when you install your app.
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/projects/tg.devtools/trunk%404849

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setup(
         'SQLAlchemy>=0.5beta1', 'PEAK-Rules',
         'sqlalchemy-migrate>=0.4.4', 'ToscaWidgets>=0.9', 
         'tw.forms>=0.9', 'DBSprockets >= 0.2, <0.5',
-        'tg.ext.repoze.who'
+        'tg.ext.repoze.who',
+        'wsgiref==0.1.2',
     ],
     entry_points='''
         [paste.global_paster_command]
