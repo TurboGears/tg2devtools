1f0e7818b9232cb376b038e3085ec649db0c9fc5
mramm <none@none>
2008-07-29 03:11:09 +0000

    --HG-- extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/projects/tg.devtools/trunk%405052

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os
 
 setup(
     name='tg.devtools',
-    version="1.9.7a2",
+    version="1.9.7a3",
     description="",
     long_description="""""",
     classifiers=[],
@@ -17,12 +17,19 @@ setup(
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Pylons>=0.9.7beta5','TurboGears2', 
-        'SQLAlchemy', 'PEAK-Rules',
-        'sqlalchemy-migrate>=0.4.4', 'ToscaWidgets>=0.9', 
-        'tw.forms>=0.9', 'DBSprockets >=0.5dev-r380',
-        'tg.ext.repoze.who', 'TurboJson>=1.2',
-        'wsgiref==0.1.2',
+    install_requires=[
+        'Pylons>=0.9.7beta5',
+        'TurboGears2', 
+        'SQLAlchemy>=0.5beta2', 
+        'PEAK-Rules',
+        'sqlalchemy-migrate>=0.4.4', 
+        'ToscaWidgets>=0.9', 
+        'tw.forms>=0.9', 
+        'DBSprockets >=0.5dev-r380',
+        'tg.ext.repoze.who', 
+        'TurboJson>=1.2',
+        'wsgiref==0.1.2', 
+        'Paste>=1.7'
     ],
     entry_points='''
         [paste.global_paster_command]
