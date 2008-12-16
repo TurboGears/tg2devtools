296909321ca0534e695e7a160694a3d175fead54
faide <none@none>
2008-12-16 12:47:01 +0000

    Next step in fixing the tests... But unfortunatly I get a deprecation error deep inside paste and I'm no Paste expert :(
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/projects/tg.devtools/trunk%405928

--- devtools/tests/test_pastetemplate.py
+++ devtools/tests/test_pastetemplate.py
@@ -5,22 +5,28 @@ from paste.deploy import loadapp
 from paste.fixture import TestApp
 from paste.script.create_distro import CreateDistroCommand
 
-testDataPath=os.path.abspath(os.path.dirname(__file__))+os.sep+'data'
+testDataPath = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        'data')
 
 app = None
 
 class MochOptions:
     simulate = False
-    overwrite=True
-    templates=['turbogears2',]
-    output_dir=testDataPath
-    list_templates=False
-    list_variables=False
-    config=None
-    inspect_files=False
-    svn_repository=False
+    overwrite = True
+    templates = ['turbogears2',]
+    output_dir = testDataPath
+    list_templates = False
+    list_variables = False
+    config = None
+    inspect_files = False
+    svn_repository = False
 
 def setup():
+    # if a test failed previously we need to cleanup the mess
+    # not mandatory but cleaner
+    #shutil.rmtree(testDataPath, ignore_errors=True)
+
     global app
     command = CreateDistroCommand('name')
     command.verbose = True
@@ -28,15 +34,30 @@ def setup():
     command.options = MochOptions()
     command.interactive=False
     command.args=['TGTest',]
-    command.args.append("sqlalchemy=%s"%True)
-    command.args.append("sqlobject=%s"%False)
-    command.args.append("auth=%s"%False)
+    command.args.append("sqlalchemy=%s" % True)
+    command.args.append("elixir=%s" % False)
+    command.args.append("sqlobject=%s" % False)
+    command.args.append("auth=%s" % False)
+
     print command
+
+    proj_dir = os.path.join(testDataPath, 'TGTest')
+
     command.templates = TurboGearsTemplate('TGTest')
-    command.create_template(TurboGearsTemplate('TGTest'), testDataPath+'/TGTest', {'package':'TGTest', 'project':'tgtest', 'egg':'tgtest', 'sqlalchemy':True, 'sqlobject':False, 'identity':False})
+    command.create_template(
+            TurboGearsTemplate('TGTest'),
+            proj_dir,
+            {
+                'package': 'TGTest',
+                'project': 'tgtest',
+                'egg': 'tgtest',
+                'sqlalchemy': True,
+                'sqlobject': False,
+                'elixir': False,
+                'auth': False
+            })
+
     command.command()
-    here_dir = os.path.dirname(os.path.abspath(__file__))
-    proj_dir = testDataPath+'/TGTest'
     
     pkg_resources.working_set.add_entry(proj_dir)
     app = loadapp('config:development.ini', relative_to=proj_dir)
@@ -45,9 +66,10 @@ def setup():
 def teardown():
     shutil.rmtree(testDataPath, ignore_errors=True)
     
-def _test_app_runs_index():
+def test_app_runs_index():
     resp = app.get('/')
     s =  resp.body
+    print s
     assert """<h2>Getting help</h2>
       <ul class="links">
         <li><a href="http://docs.turbogears.org/2.0">Documentation</a></li>
@@ -56,3 +78,4 @@ def _test_app_runs_index():
         <li><a href="http://groups.google.com/group/turbogears">Mailing
         List</a></li>
       </ul>""" in s, s
+
