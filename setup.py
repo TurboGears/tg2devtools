f5a4545ae9a245db525be0ea0ff219b0ae02e60f
mramm <none@none>
2009-02-01 18:46:36 +0000

    Appending pysqlite to install requirements for python 2.4.   (We don't support 2.3 and 2.5 has it included by default.)
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/projects/tg.devtools/trunk%406215

--- setup.py
+++ setup.py
@@ -12,6 +12,34 @@ test_requirements = ['coverage',
                     'SQLAlchemy>=0.5beta3',
                     'repoze.what-quickstart'
                     ]
+install_requirements = ['Pylons>=0.9.7beta5',
+                        'Catwalk',
+                        'TurboGears2>=2.0b3',
+
+                        'sprox',
+                        'BeautifulSoup',
+                        'Beaker>=1.1.3',
+                        'FormEncode>=1.2',
+                        'Paste>=1.7',
+                        'PEAK-Rules',
+                        'repoze.tm2',
+                        'repoze.what-quickstart',
+                        'repoze.who >= 1.0.10',
+                        'Routes>=1.10.2',
+                        'sqlalchemy-migrate>=0.4.4',
+                        'SQLAlchemy>=0.5.0beta3',
+                        'SQLAlchemy>=0.5beta3',
+                        'ToscaWidgets>=0.9',
+                        'TurboJson',
+                        'tw.forms>=0.9.2',
+                        'WebTest',
+                        'WebOb',
+                        'wsgiref==0.1.2',
+                        'zope.sqlalchemy',
+                    ],
+
+if sys.version_info[:2] == (2,4):
+   install_requires += ["pysqlite"]
 
 setup(
     name='tg.devtools',
@@ -27,32 +55,7 @@ setup(
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'Pylons>=0.9.7beta5',
-        'Catwalk',
-        'TurboGears2>=2.0b3',
-
-        'sprox',
-        'BeautifulSoup',
-        'Beaker>=1.1.3',
-        'FormEncode>=1.2',
-        'Paste>=1.7',
-        'PEAK-Rules',
-        'repoze.tm2',
-        'repoze.what-quickstart',
-        'repoze.who >= 1.0.10',
-        'Routes>=1.10.2',
-        'sqlalchemy-migrate>=0.4.4',
-        'SQLAlchemy>=0.5.0beta3',
-        'SQLAlchemy>=0.5beta3',
-        'ToscaWidgets>=0.9',
-        'TurboJson',
-        'tw.forms>=0.9.2',
-        'WebTest',
-        'WebOb',
-        'wsgiref==0.1.2',
-        'zope.sqlalchemy',
-    ],
+    install_requires = install_requirements
     entry_points='''
         [paste.global_paster_command]
         quickstart = devtools.commands.quickstart:QuickstartCommand
