cf0583d4603acb5b6f6b86f9b2b98f2d8300b4b0
jorge.vargas <none@none>
2009-02-15 23:54:28 +0000

    overhaul of tg.devtools unitesting, now it supports testing several options (although not coded), also it now runs the quickstarted project's unitests.
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/projects/tg.devtools/trunk%406319

--- devtools/tests/test_pastetemplate.py
+++ devtools/tests/test_pastetemplate.py
@@ -2,16 +2,14 @@ from devtools.pastetemplate import TurboGearsTemplate
 import os, shutil
 import pkg_resources
 from paste.deploy import loadapp
-from paste.fixture import TestApp
+from webtest import TestApp
 from paste.script.create_distro import CreateDistroCommand
 
 testDataPath = os.path.join(
         os.path.abspath(os.path.dirname(__file__)),
         'data')
 
-app = None
-
-class MochOptions:
+class MochOptions(object):
     simulate = False
     overwrite = True
     templates = ['turbogears2',]
@@ -22,65 +20,62 @@ class MochOptions:
     inspect_files = False
     svn_repository = False
 
-def setup():
-    # if a test failed previously we need to cleanup the mess
-    # not mandatory but cleaner
-    #shutil.rmtree(testDataPath, ignore_errors=True)
+class TestQuickStart(object):
+    def __init__(self,**options):
+        self.app = None
+        self.template_vars = {
+                    'package': 'TGTest',
+                    'project': 'tgtest',
+                    'egg': 'tgtest',
+                    'egg_plugins': ['PasteScript', 'Pylons', 'TurboGears2', 'tg.devtools'],
+                    'sqlalchemy': True,
+                    'sqlobject': False,
+                    'elixir': False,
+                    'auth': True,
+                    'geo': False
+        }
 
-    global app
-    command = CreateDistroCommand('name')
-    command.verbose = True
-    command.simulate = False
-    command.options = MochOptions()
-    command.interactive=False
-    command.args=['TGTest',]
-    command.args.append("sqlalchemy=%s" % True)
-    command.args.append("elixir=%s" % False)
-    command.args.append("sqlobject=%s" % False)
-    command.args.append("auth=%s" % False)
-    command.args.append("geo=%s" % False)
+    def setUp(self):
+        command = CreateDistroCommand('TGQuickStartUnitTest')
+        command.verbose = False
+        command.simulate = False
+        command.options = MochOptions()
+        command.interactive=False
+        command.args=['TGTest',]
 
-    print command
+        proj_dir = os.path.join(testDataPath, 'TGTest')
+        command.create_template(
+                TurboGearsTemplate('TGTest'),
+                proj_dir,
+                self.template_vars)
+        command.command()
+        
+        pkg_resources.working_set.add_entry(proj_dir)
+        self.app = loadapp('config:development.ini', relative_to=proj_dir)
+        self.app = TestApp(self.app)
 
-    proj_dir = os.path.join(testDataPath, 'TGTest')
+    def tearDown(self):
+        shutil.rmtree(testDataPath, ignore_errors=True)
+        
+    def test_index(self):
+        resp = self.app.get('/')
+        assert 'Now Viewing: index' in resp
 
-    command.templates = TurboGearsTemplate('TGTest')
-    command.create_template(
-            TurboGearsTemplate('TGTest'),
-            proj_dir,
-            {
-                'package': 'TGTest',
-                'project': 'tgtest',
-                'egg': 'tgtest',
-                'egg_plugins': ['PasteScript', 'Pylons', 'TurboGears2', 'tg.devtools'],
-                'sqlalchemy': True,
-                'sqlobject': False,
-                'elixir': False,
-                'auth': False,
-                'geo': False
-            })
-    print "FSFSFSDFSDFSDFOSIDUGOSFG"
-    command.command()
-    
-    pkg_resources.working_set.add_entry(proj_dir)
-    print proj_dir
-    app = loadapp('config:development.ini', relative_to=proj_dir)
-    app = TestApp(app)
+    def test_login(self):
+        resp = self.app.get('/login')
+        assert '<div id="loginform">' in resp
 
-def teardown():
-    shutil.rmtree(testDataPath, ignore_errors=True)
-    
-def test_app_runs_index():
-    resp = app.get('/')
-    s =  resp.body
-    print s
-    assert """<h2>Get Started with TG2</h2>
-      <ul class="links">
-        <li>
-          <span><a href="/about">About this page</a> A quick guide to this TG2 site </span>
-        </li>
-        <li><a href="http://www.turbogears.org/2.0/docs/">TG2 Documents</a> - Read everything in the Getting Started section</li>
-        <li><a href="http://docs.turbogears.org/1.0">TG1 docs</a> (still useful, although a lot has changed for TG2) </li>
-        <li><a href="http://groups.google.com/group/turbogears"> Join the TG Mail List</a> for general TG use/topics  </li>
-      </ul>""" in s, s
+    def test_admin(self):
+        resp = self.app.get('/admin/')
+        print resp
+        assert 'This is a fully-configurable administrative tool to help you administer your website.' in resp
+        assert '<li><a href="group/">Groups</a></li><li><a href="user/">Users</a></li><li><a href="permission/">Permissions</a></li>' in resp
 
+#now we run the quickstarted project nosetests
+#FIXME, we need a better implementation
+import nose
+cls = TestQuickStart()
+cls.setUp()
+testspath = os.path.join(testDataPath,'TGTest','tgtest','tests')
+nose.main(argv=['-w',testspath],exit=False)
+cls.tearDown()
