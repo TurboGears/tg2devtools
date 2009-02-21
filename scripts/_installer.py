9e6dd50d260c570a592f0380ea3eb007f4245614
jorge.vargas <none@none>
2009-02-21 04:01:11 +0000

    adding a virtualenv bootstrap script, also scripts dir that should only have useful scripts for the development of tg itself
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/projects/tg.devtools/trunk%406368

---
+++ scripts/_installer.py
@@ -0,0 +1,62 @@
+import os 
+
+def extend_parser(parser):
+    parser.add_option(
+            '--trunk',
+            dest='trunk',
+            action='store_true',
+            default=False,
+            help='Install TurboGears in development mode, useful for when you want to contribute back patches (experimental, requires pip)')
+    parser.add_option(
+            '--requrements',
+            dest='requirement_file',
+            metavar='requirements.txt',
+            default=None,
+            help='URL or path to the pip requirement files to use')
+    parser.add_option(
+            '--pip',
+            dest='use_pip',
+            action='store_true',
+            default=False,
+            help='Use pip instead of easy_install (experimental)')
+
+def execute(script,*args):
+    print 'Running script'
+    print script
+    print 20*'-'
+    subprocess.Popen(script % locals(),shell=True)
+
+def after_install(options, home_dir):
+    print options
+    if sys.platform == 'win32':
+        bin = 'Scripts'
+    else:
+        bin = 'bin'
+
+    if options.requirement_file:
+        print "Sorry option not supported yet"
+        return
+
+    print 'installing turbogears, this will take a while....'
+    if options.use_pip:
+        if options.trunk:
+            install_cmd = """
+            pip install -e svn+http://svn.turbogears.org/trunk
+            pip install -e svn+http://svn.turbogears.org/projects/tg.devtools/trunk
+            """
+        else:
+            install_cmd = 'pip install -i http://www.turbogears.org/2.0/downloads/current/index tg.devtools'
+
+        execute("""
+        cd %(home_dir)s
+        source %(bin)s/activate
+        easy_install pip
+        %(install_cmd)s
+        """ % locals())
+    else:#use easy_install
+        execute("""
+        cd %(home_dir)s
+        source %(bin)s/activate
+        easy_install -i http://www.turbogears.org/2.0/downloads/current/index tg.devtools
+        """ % locals())
+
