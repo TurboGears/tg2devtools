4af4f7fd5d76daa2dd569024f2f50f54f8ff1118
Michael J. Pedersen <m.pedersen@icelus.org>
2012-04-05 00:27:51 -0400

    Changing to use 2.1.5 urls.

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='nose.collector',
     tests_require = test_requirements,
     dependency_links=[
-        "http://www.turbogears.org/2.1/downloads/current/"
+        "http://tg.gy/215/"
         ]
 )
